a4ba43ee623584e1969d1c5f0ce23432c5df6ff5
Ruchi Bhandari <98799210+ruchibhandarigithub@users.noreply.github.com>
2023-05-27 06:37:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-//your code here
+//yofunction sortBands(bandNames) {
+  // Remove articles from band names and create a new array
+  const cleanedNames = bandNames.map(name => name.replace(/^(a |an |the )/i, ''));
+
+  // Sort the cleaned names array in lexicographic order
+  const sortedNames = cleanedNames.sort((a, b) => a.localeCompare(b));
+
+  // Generate the HTML string with the sorted band names
+  let html = '<ul id="band">';
+  sortedNames.forEach(name => {
+    html += `<li>${name}</li>`;
+  });
+  html += '</ul>';
+
+  return html;
+}
 
